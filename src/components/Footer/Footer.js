0b74dd102838012a03fd68a43b9b28860ebf5c75
CStokan <81194869+CStokan@users.noreply.github.com>
2023-04-05 13:28:11 -0400

    Redeploying

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>Innovating one project at a time</Slogan>
+          <Slogan>Technology is best when it brings people together.</Slogan>
         </CompanyContainer>
         <SocialContainer>
           <SocialIcons href='https://github.com/CStokan'>
